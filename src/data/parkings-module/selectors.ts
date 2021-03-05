413787f452baa6dd7f3eb21cfbe35335cf976c55
PazReshef <pazreshef@gmail.com>
2021-03-05 09:33:52 +0200

    add search input

--- src/data/parkings-module/selectors.ts
+++ src/data/parkings-module/selectors.ts
@@ -1,4 +1,16 @@
 import { RootState } from "../configureStore";
+import {createSelector} from "reselect";
+
+export const getProps = (state: RootState, props: any) => props;
 
 export const parkingsSelector = (state: RootState) =>
   state.Parkings.parkings;
+
+
+export const parkingsWithFilterSelector = createSelector(
+    [parkingsSelector, getProps], (parkings, props) => {
+        return parkings.filter(p => {
+            return (p.description.includes(props.searchText) || p.address.includes(props.searchText))
+        })
+    }
+);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import ParkingListItem from '../components/ParkingListItem';
 import { useState, useEffect } from 'react';
-import { Message, getMessages } from '../data/messages';
 import {
   IonContent,
   IonHeader,
@@ -11,25 +10,27 @@ import {
   IonTitle,
   IonToolbar,
   useIonViewWillEnter,
-  IonText
+  IonText, IonSearchbar
 } from '@ionic/react';
 import './Home.css';
 import firebase from 'firebase';
 import {useDispatch, useSelector} from "react-redux";
 import {getParkings} from "../data/parkings-module/actions";
-import {parkingsSelector} from "../data/parkings-module/selectors";
+import {parkingsSelector, parkingsWithFilterSelector} from "../data/parkings-module/selectors";
+import {RootState} from "../data/configureStore";
 import {Parking} from "../data/parkings-module/types";
 
 
 const Home: React.FC = () => {
 
   const [displayName, setDisplayName] = useState<string>();
+  const [searchText, setSearchText] = useState<string>("");
 
   firebase.auth().onAuthStateChanged(()=>{
     setDisplayName(firebase.auth().currentUser?.displayName?.toString());
   });
 
-  const parkings = useSelector(parkingsSelector);
+  const parkings = useSelector((state: RootState) => parkingsWithFilterSelector(state, {searchText}));
 
   const refresh = (e: CustomEvent) => {
     setTimeout(() => {
@@ -51,7 +52,7 @@ const Home: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
         <IonRefresher slot="fixed" onIonRefresh={refresh}>
-          <IonRefresherContent></IonRefresherContent>
+          <IonRefresherContent />
         </IonRefresher>
 
         <IonHeader collapse="condense">
@@ -62,6 +63,7 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
 
+        <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} animated placeholder={"חפש חניה"}/>
         <IonList>
           {parkings.map(p => <ParkingListItem key={p.id} parking={p} />)}
         </IonList>
